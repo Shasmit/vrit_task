8d8840e9da9f8e38de7fcd20f99bf9ce709861ea
Shasmit <shasmitbasnet606@gmail.com>
2024-04-21 12:36:07 +0545

    completed task

--- lib/home.dart
+++ lib/home.dart
@@ -14,6 +14,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final urlController = TextEditingController();
   final shortUrlController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
   bool isShortened = false;
   @override
   Widget build(BuildContext context) {
@@ -23,7 +25,7 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(
-          'URL Shortner',
+          'URL Shortener',
           style: TextStyle(
             color: Colors.black,
             fontFamily: 'PoppinsSemiBold',
@@ -35,204 +37,216 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: screenHeight * 0.05,
-              ),
-              const Text(
-                'Shorten your URL',
-                style: TextStyle(
-                  color: Colors.blueAccent,
-                  fontFamily: 'PoppinsSemiBold',
-                  fontSize: 20,
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: screenHeight * 0.05,
                 ),
-              ),
-              SizedBox(
-                height: screenHeight * 0.02,
-              ),
-              const Text(
-                'Enter your URL',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'PoppinsMedium',
-                  fontSize: 16,
+                const Text(
+                  'Shorten your URL',
+                  style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontFamily: 'PoppinsSemiBold',
+                    fontSize: 20,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: screenHeight * 0.02,
-              ),
-              SizedBox(
-                width: screenWidth * 0.8,
-                child: TextFormField(
-                  controller: urlController,
-                  style: const TextStyle(
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+                const Text(
+                  'Enter your URL',
+                  style: TextStyle(
                     color: Colors.black,
                     fontFamily: 'PoppinsMedium',
-                    fontSize: 15,
-                  ),
-                  decoration: InputDecoration(
-                    hintText: 'Enter URL',
-                    hintStyle: const TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'PoppinsMedium',
-                      fontSize: 16,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: screenHeight * 0.02,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    HttpsServices().postUrl(urlController.text, context);
-                    if (StaticVariables.shortUrl != '') {
-                      isShortened = true;
-                      shortUrlController.text = StaticVariables.shortUrl;
-                    }
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 50,
-                    vertical: 10,
-                  ),
-                  backgroundColor: Colors.blueAccent,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                child: const Text(
-                  'Shorten',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'PoppinsSemiBold',
                     fontSize: 16,
                   ),
                 ),
-              ),
-              isShortened
-                  ? Column(
-                      children: [
-                        SizedBox(
-                          height: screenHeight * 0.05,
-                        ),
-                        const Text(
-                          'Shortened URL :',
-                          style: TextStyle(
-                            color: Colors.blueAccent,
-                            fontFamily: 'PoppinsMedium',
-                            fontSize: 16,
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+                SizedBox(
+                  width: screenWidth * 0.8,
+                  child: TextFormField(
+                    controller: urlController,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'PoppinsMedium',
+                      fontSize: 15,
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter an URL';
+                      }
+
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Enter URL',
+                      hintStyle: const TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'PoppinsMedium',
+                        fontSize: 16,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      setState(() {
+                        HttpsServices().postUrl(urlController.text, context);
+                        if (StaticVariables.shortUrl != '') {
+                          isShortened = true;
+                          shortUrlController.text = StaticVariables.shortUrl;
+                        }
+                      });
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 10,
+                    ),
+                    backgroundColor: Colors.blueAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'Shorten',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'PoppinsSemiBold',
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                isShortened
+                    ? Column(
+                        children: [
+                          SizedBox(
+                            height: screenHeight * 0.05,
                           ),
-                        ),
-                        SizedBox(
-                          height: screenHeight * 0.02,
-                        ),
-                        SizedBox(
-                          width: screenWidth * 0.8,
-                          child: TextFormField(
-                            readOnly: true,
-                            style: const TextStyle(
-                              color: Colors.black,
+                          const Text(
+                            'Shortened URL :',
+                            style: TextStyle(
+                              color: Colors.blueAccent,
                               fontFamily: 'PoppinsMedium',
-                              fontSize: 15,
+                              fontSize: 16,
                             ),
-                            initialValue: StaticVariables.shortUrl,
-                            decoration: InputDecoration(
-                              hintText: 'Shortened URL',
-                              hintStyle: const TextStyle(
+                          ),
+                          SizedBox(
+                            height: screenHeight * 0.02,
+                          ),
+                          SizedBox(
+                            width: screenWidth * 0.8,
+                            child: TextFormField(
+                              readOnly: true,
+                              style: const TextStyle(
                                 color: Colors.black,
                                 fontFamily: 'PoppinsMedium',
-                                fontSize: 16,
+                                fontSize: 15,
                               ),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
+                              initialValue: StaticVariables.shortUrl,
+                              decoration: InputDecoration(
+                                hintText: 'Shortened URL',
+                                hintStyle: const TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'PoppinsMedium',
+                                  fontSize: 16,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: screenHeight * 0.02,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            ElevatedButton(
-                              onPressed: () {
-                                setState(() {
-                                  FlutterClipboard.copy(
-                                      StaticVariables.shortUrl);
-
+                          SizedBox(
+                            height: screenHeight * 0.02,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {
                                   setState(() {
-                                    isShortened = false;
-                                    urlController.clear();
+                                    FlutterClipboard.copy(
+                                        StaticVariables.shortUrl);
+
+                                    setState(() {
+                                      isShortened = false;
+                                      urlController.clear();
+                                    });
                                   });
-                                });
-                              },
-                              style: ElevatedButton.styleFrom(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 50,
-                                  vertical: 10,
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 50,
+                                    vertical: 10,
+                                  ),
+                                  backgroundColor: Colors.blueAccent,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
                                 ),
-                                backgroundColor: Colors.blueAccent,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                              child: const Text(
-                                'Copy',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: 'PoppinsSemiBold',
-                                  fontSize: 15,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: screenWidth * 0.05,
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                setState(() {
-                                  urlController.clear();
-                                  isShortened = false;
-                                });
-                                Navigator.pushNamed(
-                                  context,
-                                  AppRoute.qrRoute,
-                                );
-                              },
-                              style: ElevatedButton.styleFrom(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 50,
-                                  vertical: 10,
-                                ),
-                                backgroundColor: Colors.blueAccent,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
+                                child: const Text(
+                                  'Copy',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'PoppinsSemiBold',
+                                    fontSize: 15,
+                                  ),
                                 ),
                               ),
-                              child: const Text(
-                                'View Qr',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: 'PoppinsSemiBold',
-                                  fontSize: 15,
+                              SizedBox(
+                                width: screenWidth * 0.05,
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    urlController.clear();
+                                    isShortened = false;
+                                  });
+                                  Navigator.pushNamed(
+                                    context,
+                                    AppRoute.qrRoute,
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 50,
+                                    vertical: 10,
+                                  ),
+                                  backgroundColor: Colors.blueAccent,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'View Qr',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'PoppinsSemiBold',
+                                    fontSize: 15,
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    )
-                  : Container(),
-            ],
+                            ],
+                          ),
+                        ],
+                      )
+                    : Container(),
+              ],
+            ),
           ),
         ),
       ),
